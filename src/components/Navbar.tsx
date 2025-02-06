634a0e789043275be565280fe2d561143e790372
Behnam <behnam.sep@gmail.com>
2025-02-06 21:53:00 +0100

    complete the nav component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,56 @@
 import React from 'react';
-import { CiSearch } from 'react-icons/ci';
+import { IoSearch } from 'react-icons/io5';
 import { FaRegUser } from 'react-icons/fa6';
 import { SlBasket } from 'react-icons/sl';
 import { FaRegHeart } from 'react-icons/fa';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { IoMenuSharp } from 'react-icons/io5';
 
 function Navbar() {
+	const navButtons = [
+		{ icon: IoSearch, label: 'Search' },
+		{ icon: FaRegHeart, label: 'Favorites' },
+		{ icon: FaRegUser, label: 'Profile' },
+	];
+
 	return (
-		<nav>
-			<div className="flex h-16 w-full min-w-[300px] items-center justify-between bg-gray-100">
-				<div>
-					<ul className="bg-red-500 flex items-center justify-center gap-3 w-[30%]">
-						<li>Women</li>
-						<li>Men</li>
-						<li>Kids</li>
-					</ul>
+		<nav className="flex w-full items-center justify-center">
+			<div className="mx-auto h-12 w-full max-w-[1920px] min-w-[300px] flex-col border-b-2 border-b-gray-300 bg-gray-100 lg:h-32">
+				<div className="flex h-full w-full items-center justify-between lg:h-1/2">
+					<span className="absolute font-display left-2 text-2xl font-semibold md:left-1/2 md:-translate-x-1/2 lg:top-4 lg:text-3xl">
+						BENSTORE
+					</span>
+					<button className="hidden h-10 w-36 translate-x-6 items-center justify-center gap-2 rounded-xs md:flex">
+						<FaPhoneAlt />
+						<span> +1-123456789</span>
+					</button>
+
+					<div className="flex w-[170px] items-center justify-center gap-2">
+						{navButtons.map((button, index) => (
+							<button
+								key={index}
+								className="hidden h-10 w-10 items-center justify-center hover:cursor-pointer lg:flex"
+							>
+								<button.icon />
+							</button>
+						))}
+					</div>
+					<IoMenuSharp className="h-10 w-10 -translate-x-2 hover:cursor-pointer lg:hidden" />
 				</div>
-				<span className="text-center text-3xl font-semibold">
-					BENSTORE
-				</span>
-				<div className="flex items-center justify-center">
-					<ul className="flex items-center justify-center gap-3">
-						<li>HomePage</li>
-						<li>About</li>
-						<li>Contact</li>
-						<li>Stores</li>
+				<div className="relative hidden h-1/2 w-full items-center justify-center pl-5 lg:flex">
+					<ul className="ml-5 flex items-center justify-center gap-16">
+						<li>Home</li>
+						<li>BackPack</li>
+						<li>Messenger Bags</li>
+						<li>Brifcase</li>
+						<li>Totes</li>
+						<li>Clutches</li>
 					</ul>
-					<div className="flex items-center justify-center gap-2">
-						<CiSearch />
-						<FaRegUser />
-						<FaRegHeart />
-						<SlBasket />
+					<div className="absolute right-6 flex items-center justify-center gap-2">
+						<SlBasket className="text-xl" />
+						<span className="flex h-7 w-7 items-center justify-center rounded-full bg-gray-800 text-center text-white">
+							0
+						</span>
 					</div>
 				</div>
 			</div>
