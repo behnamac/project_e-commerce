747ab11a81b4edb4037314e53feefcdae2f0e623
Behnam <behnam.sep@gmail.com>
2025-02-06 22:06:31 +0100

    feat(navbar): add phone contact button with icon and number in top section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,12 +12,20 @@ function Navbar() {
 		{ icon: FaRegHeart, label: 'Favorites' },
 		{ icon: FaRegUser, label: 'Profile' },
 	];
+	const navLinks = [
+		{ href: '/', label: 'Home' },
+		{ href: '/backPack', label: 'BackPack' },
+		{ href: '/messengerBags', label: 'Messenger Bags' },
+		{ href: '/brifcase', label: 'Brifcase' },
+		{ href: '/totes', label: 'Totes' },
+		{ href: '/clutches', label: 'Clutches' },
+	];
 
 	return (
 		<nav className="flex w-full items-center justify-center">
 			<div className="mx-auto h-12 w-full max-w-[1920px] min-w-[300px] flex-col border-b-2 border-b-gray-300 bg-gray-100 lg:h-32">
 				<div className="flex h-full w-full items-center justify-between lg:h-1/2">
-					<span className="absolute font-display left-2 text-2xl font-semibold md:left-1/2 md:-translate-x-1/2 lg:top-4 lg:text-3xl">
+					<span className="font-display absolute left-2 text-2xl font-semibold md:left-1/2 md:-translate-x-1/2 lg:top-4 lg:text-3xl">
 						BENSTORE
 					</span>
 					<button className="hidden h-10 w-36 translate-x-6 items-center justify-center gap-2 rounded-xs md:flex">
@@ -39,12 +47,19 @@ function Navbar() {
 				</div>
 				<div className="relative hidden h-1/2 w-full items-center justify-center pl-5 lg:flex">
 					<ul className="ml-5 flex items-center justify-center gap-16">
-						<li>Home</li>
-						<li>BackPack</li>
-						<li>Messenger Bags</li>
-						<li>Brifcase</li>
-						<li>Totes</li>
-						<li>Clutches</li>
+						{navLinks.map((link, index) => (
+							<li
+								key={index}
+								className="flex h-full items-center justify-center"
+							>
+								<a
+									href={link.href}
+									className="flex h-full items-center justify-center"
+								>
+									{link.label}
+								</a>
+							</li>
+						))}
 					</ul>
 					<div className="absolute right-6 flex items-center justify-center gap-2">
 						<SlBasket className="text-xl" />
